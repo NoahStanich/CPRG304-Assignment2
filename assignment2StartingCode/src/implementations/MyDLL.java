1bff599ee0481c06c978857d682decadec105ce5
Sasha Maksymenko <oleksandr.maksymenko@edu.sait.ca>
2025-11-23 12:52:32 -0700

    Updated MyDLL
    added few methods to MyDLL to make implementation of the MyQueue easier.
    Also added some documentation to the methods.

--- assignment2StartingCode/src/implementations/MyDLL.java
+++ assignment2StartingCode/src/implementations/MyDLL.java
@@ -4,13 +4,18 @@ import java.util.NoSuchElementException;
 
 import utilities.Iterator;
 import utilities.ListADT;
-
+/**
+ * Double Linked List class. Implementation of ListADT.
+ * Creates and manages Double Linked List
+ * @param <E> - type of data stored in the list.
+ */
 public class MyDLL<E> implements ListADT<E> {
 
-	// Inner Node Class
+	// Inner Single Node Class
 	@SuppressWarnings("hiding")
 	public class MyDLLNode<E> {
 		
+		// Fields
 		private E data;
 		private MyDLLNode<E> next;
 		private MyDLLNode<E> prev;
@@ -24,43 +29,24 @@ public class MyDLL<E> implements ListADT<E> {
 		public MyDLLNode(E element) {
 			this.data = element;
 		}
-		
-		// Getters and Setters
-		public E getData() {
-			return data;
-		}
-
-		public void setData(E data) {
-			this.data = data;
-		}
-
-		public MyDLLNode<E> getNext() {
-			return next;
-		}
-
-		public void setNext(MyDLLNode<E> next) {
-			this.next = next;
-		}
-
-		public MyDLLNode<E> getPrev() {
-			return prev;
-		}
-
-		public void setPrev(MyDLLNode<E> prev) {
-			this.prev = prev;
-		}
 	}
 	
+	/**
+	 * Inner DLL Iterator class.
+	 */
 	public class DLLIterator implements Iterator<E>{
 		
+		// Fields
 		private E[] copyOfDLL;
 		private int pos = 0;
 		
+		// Constructor (makes a copy of the list) 
 		@SuppressWarnings("unchecked")
 		public DLLIterator() {
 			copyOfDLL = (E[]) new Object[size];
 			copyOfDLL = toArray(copyOfDLL);
 		}
+		
 		@Override
 		public boolean hasNext() {
 			return pos < copyOfDLL.length;
@@ -82,6 +68,14 @@ public class MyDLL<E> implements ListADT<E> {
 	private MyDLLNode<E> tail;
 	private int size = 0;
 	
+	public MyDLLNode<E> getHead(){
+		return head;
+	}
+	
+	public MyDLLNode<E> getTail(){
+		return tail;
+	}
+	
 	// Parameterless constructor
 	public MyDLL() {
 		
@@ -92,7 +86,15 @@ public class MyDLL<E> implements ListADT<E> {
 		createFirst(element);
 	}
 	
-	public boolean createFirst(E element) {
+	/**
+	 * Method that creates new DLL using the E element passed as a first element.
+	 * @param element to be the first element in the list.
+	 * @return true if successful.
+	 * @throws NullPointerException if element passed is null.
+	 */
+	public boolean createFirst(E element) throws NullPointerException {
+		if(element == null) throw new NullPointerException();
+		
 		this.head = this.tail = new MyDLLNode<>(element);
 		size = 1;
 		
@@ -109,39 +111,91 @@ public class MyDLL<E> implements ListADT<E> {
 		this.head = this.tail = null;
 		size = 0;
 	}
-
+	
+	/**
+	 * Method to insert the element in the first position of the list. 
+	 * @param element to add first in the list
+	 * @return true if successful
+	 * @throws NullPointerException if element passed is null
+	 */
+	public boolean addFirst(E element) throws NullPointerException{
+		if(element == null) throw new NullPointerException();
+		
+		if( size == 0 ) {
+			return createFirst(element);
+		}
+		
+		MyDLLNode<E> newNode = new MyDLLNode<>(element);
+		
+		head.prev = newNode;
+		newNode.next = head;
+		head = newNode;
+		size++;
+		
+		return true;
+	}
+	
+	/**
+	 * Method to remove first Node from DLL
+	 * @return element from the Node removed
+	 */
+	public E removeFirst() {
+		E element = head.data;
+		
+		head = head.next;
+		head.prev.next = null;
+		head.prev = null;
+		size--;
+		
+		return element;
+	}
+	
+	/**
+	 * Method to remove the last Node from the list.
+	 * @return Element deleted
+	 */
+	public E removeLast(){
+		E element = tail.data;
+		
+		tail = tail.prev;
+		tail.next.prev = null;
+		tail.next = null;
+		
+		size--;
+		
+		return element;
+	}
+	
+	/**
+	 * Method that allows to insert an element to a certain index in the list.
+	 * @param index where the element should be inserted
+	 * @param toAdd element to be inserted in the list
+	 * @returns true if successful
+	 * @throws NullPointerException if element passed is null.
+	 */
 	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
 		if(toAdd == null) throw new NullPointerException();
 		if(index < 0 || index > size) throw new IndexOutOfBoundsException();
-		
-		// if list is yet empty 
-		if(size == 0) {
-			return createFirst(toAdd);
+				
+		// if element has to be added in the first position
+		if(index == 0) {
+			return addFirst(toAdd);
 		}
 		
-		// if add in the end
+		// if element to be added at the end
 		if(index == size) {
 			return add(toAdd);
 		}
 		
+		// other cases (element has to be added in between other elements)
 		MyDLLNode<E> newNode = new MyDLLNode<>(toAdd);
-		
-		if(index == 0) {
-			newNode.next = head;
-			head.prev = newNode;
-			head = newNode;
-			size++;
-			
-			return true;
-		}
-				
-		// other cases
+						
 		MyDLLNode<E> curr = head;
 		for( int i = 0 ; i < index ; i++ ) {
 			curr = curr.next;
 		}
-
+		
 		curr.prev.next = newNode;
 		newNode.prev = curr.prev;
 		newNode.next = curr;
@@ -152,15 +206,24 @@ public class MyDLL<E> implements ListADT<E> {
 		return true;
 	}
 
+	/**
+	 * Method to add a new element to the end of the list.
+	 * @param toAdd element to be added in the list.
+	 * @return true if successful.
+	 * @throws NullPointerException if element passed is null.
+	 */
 	@Override
 	public boolean add(E toAdd) throws NullPointerException {
 		if(toAdd == null) throw new NullPointerException();
 		
+		// if list is yet empty.
 		if(size == 0) {
 			return createFirst(toAdd);
 		}
 		
+		// otherwise
 		MyDLLNode<E> newNode = new MyDLLNode<>(toAdd);
+		
 		this.tail.next = newNode;
 		newNode.prev = this.tail;
 		
@@ -170,6 +233,12 @@ public class MyDLL<E> implements ListADT<E> {
 		return true;
 	}
 
+	/**
+	 * Method that extends DLL with another DLL 
+	 * @param toAdd list that extends the original DLL
+	 * @return true if successful
+	 * @throws NullPointerException if new list passed contains null
+	 */
 	@Override
 	public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
 		Iterator<? extends E> it = toAdd.iterator();
@@ -188,18 +257,25 @@ public class MyDLL<E> implements ListADT<E> {
 		return true;
 	}
 
+	/**
+	 * Method to retrieve an element based on it's index (without deleting it).
+	 * @param index of the Node containing data.
+	 * @return element of the node at the index specified.
+	 * @throws IndexOutOfBoundsException if index provided is out of range <code>index < 0 || index >= size</code>
+	 */
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
 		if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
 		
+		// quick check if it is head or tail that needs to be returned
 		if(index == 0) {
 			return head.data;
 		}
-		
 		if(index == size - 1) {
 			return tail.data;
 		}
 		
+		// otherwise iterate until index is reached
 		MyDLLNode<E> curr = head;
 		for( int i = 0 ; i < index; i++ ) {
 			curr = curr.next;
@@ -208,32 +284,34 @@ public class MyDLL<E> implements ListADT<E> {
 		return curr.data;
 	}
 
+	/**
+	 * Method to remove a Node from the List based on it's index
+	 * @param index of the Node that needs to be deleted
+	 * @throws IndexOutOfBoundsException if <code>index < 0 || index >= size</code>
+	 * @return data from the deleted Node.
+	 */
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
 		if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
 		
 		E element;
 		
+		//Edge cases
+		// if there is only one element in the list.
 		if(size == 1) {
 			element = head.data;
 			clear();	
 			return element;
 		}
 		
+		// if the first element has to be removed
 		if(index == 0) {
-			 element = head.data;
-			 head = head.next;
-			 head.prev = null;
-			 size--;
-			 return element;
+			 return removeFirst();
 		}
-				
+		
+		//if the last element has to be removed
 		if(index == size - 1) {
-			element = tail.data;
-			tail = tail.prev;
-			tail.next = null;
-			size--;
-			return element;
+			return removeLast();
 		}
 		
 		MyDLLNode<E> curr = head;
@@ -244,39 +322,47 @@ public class MyDLL<E> implements ListADT<E> {
 		element = curr.data;
 		curr.prev.next = curr.next;
 		curr.next.prev = curr.prev;
+		curr.next = null;
+		curr.prev = null;
+		
 		size--;
 		
 		return element;
 	}
 
+	/**
+	 * Method to remove first instance of the element passed
+	 * @param toRemove instance of the element to be removed (if found)
+	 * @throws NullPointerException if toRemove element passed is null.\
+	 * @return data that is being removed
+	 */
 	@Override
 	public E remove(E toRemove) throws NullPointerException {
 		if(toRemove == null) throw new NullPointerException();
 		
-		if(size == 0) {
-			return toRemove;
-		}
-		if(size == 1 && head.data == toRemove) {
-			clear();
-			return toRemove;
-		}
-		
-		if(head.data.equals(toRemove)) {
-			head = head.next;
-			head.prev = null;
-			size--;
-			return toRemove;
-		}
-		
-		if(tail.data.equals(toRemove)) {
-			tail = tail.prev;
-			tail.next = null;
-			size--;
-			return toRemove;
-		}
-		
-		MyDLLNode<E> curr = head;
 		E element = null;
+		
+		// Edge cases
+		// if list is yet empty.
+		if(size == 0) {
+			return element;
+		}
+		
+		// if list contains just this one element.
+		if(size == 1 && head.data.equals(toRemove)) {
+			element = head.data;
+
+			clear();
+			return element;
+		}
+		
+		// if head contains the element.
+		if(head.data.equals(toRemove)) {
+			return removeFirst();
+		}
+		
+		// Otherwise iterate through the list until first instance of the element is found
+		MyDLLNode<E> curr = head;
 		
 		for(int i = 0 ; i < size ; i++ ){
 			if(curr.data.equals(toRemove)) {
@@ -286,15 +372,31 @@ public class MyDLL<E> implements ListADT<E> {
 			curr = curr.next;
 		}
 		
-		if(element != null) {
-			curr.prev.next = curr.next;
-			curr.next.prev = curr.prev;
-			size--;
+		// check if element was found
+		if(element != null ) {
+			
+			// make sure it is no in the tail (curr.next will throw nullPointer)
+			if(curr != tail) {
+				curr.prev.next = curr.next;
+				curr.next.prev = curr.prev;
+				size--;
+			} else {
+				removeLast();
+			}
+				
 		}
 		
 		return element;
 	}
 
+	/**
+	 * Method to change data inside the Node located at the index specified
+	 * @param index The index of the Node who's data has to be changed.
+	 * @param toChange new data for the Node
+	 * @throws NullPointerException if the toChange data element is null
+	 * @throws IndexOutOfBoundsException if index specified is out of range <code>index < 0 || index >= size</code>
+	 * @return Data that was previously stored in the Node
+	 */
 	@Override
 	public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
 		if(toChange == null) throw new NullPointerException();
@@ -316,6 +418,12 @@ public class MyDLL<E> implements ListADT<E> {
 		return size == 0;
 	}
 
+	/**
+	 * Method to check if DLL contains instance of the data provided in the arguments
+	 * @param toFind Data to be checked for existence in the DLL
+	 * @throws NullPointerException if toFind data provided is null
+	 * @return true if data found in the list, false if not.
+	 */
 	@Override
 	public boolean contains(E toFind) throws NullPointerException {
 		if(toFind == null) throw new NullPointerException();
