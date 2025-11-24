45a5face165ca1b9d4d028ca054e644aacc03f14
Maxwell9-9 <Chukwudumebi.Anaruagu@edu.sait.ca>
2025-11-24 11:12:46 -0700

    blah blah blah

--- assignment2StartingCode/src/implementations/MyStack.java
+++ assignment2StartingCode/src/implementations/MyStack.java
@@ -1,64 +1,52 @@
 package implementations;
 
+
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-interface StackADT<E> {
-    void clear();
-    int depth();
-    boolean push(E element) throws NullPointerException;
-    E pop();
-    boolean isEmpty();
-    E peek();
-}
+import utilities.StackADT;
 
-public abstract class MyStack<E> implements StackADT<E> {
-    
-    private ArrayList<E> stackList;
-    
+/**
+ * Stack implementation backed by MyArrayList.
+ */
+public class MyStack<E> implements StackADT<E> {
+
+    private MyArrayList<E> stackList;
+
     public MyStack() {
-        stackList = new ArrayList<E>();
-        clear();
+        stackList = new MyArrayList<E>();
     }
 
     @Override
     public void clear() {
         stackList.clear();
     }
-    
+
     @Override
     public int depth() {
         return stackList.size();
     }
-    
+
     @Override
     public boolean push(E element) throws NullPointerException {
-        if (element != null) {
-            stackList.add(element);
-            return true;
-        }
-        
-        throw new NullPointerException();
+        if (element == null) throw new NullPointerException();
+        stackList.add(element);
+        return true;
     }
-    
+
+    @Override
     public boolean pushMany(ArrayList<E> arr) throws NullPointerException {
-        if (arr != null) {
-            for (E element : arr) {
-                if (element == null) {
-                    throw new NullPointerException();
-                }
-                stackList.add(element);
-            }
-            return true;
+        if (arr == null) throw new NullPointerException();
+        for (E e : arr) {
+            if (e == null) throw new NullPointerException();
+            stackList.add(e);
         }
-        throw new NullPointerException();
+        return true;
     }
 
     @Override
     public E pop() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
+        if (isEmpty()) throw new NoSuchElementException();
         return stackList.remove(stackList.size() - 1);
     }
 
@@ -69,9 +57,7 @@ public abstract class MyStack<E> implements StackADT<E> {
 
     @Override
     public E peek() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
+        if (isEmpty()) throw new NoSuchElementException();
         return stackList.get(stackList.size() - 1);
     }
 }
