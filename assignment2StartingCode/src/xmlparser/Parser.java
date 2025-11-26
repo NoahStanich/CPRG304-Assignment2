4b71a49093e3f8ffbdb60550b9d2771fdf4df7be
Penncilk <Jude.Uyeno@edu.sait.ca>
2025-11-26 04:08:38 -0700

    Completely remade the XMLParser

--- assignment2StartingCode/src/xmlparser/Parser.java
+++ assignment2StartingCode/src/xmlparser/Parser.java
@@ -13,56 +13,124 @@
 
 package xmlparser;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamReader;
+//import javax.xml.stream.XMLInputFactory;
+//import javax.xml.stream.XMLStreamReader;
 import implementations.MyStack;
 import implementations.MyQueue;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;   
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 	public static void main(String[] args) {
 		try {
-			XMLInputFactory factory = XMLInputFactory.newInstance();
-			XMLStreamReader reader = factory.createXMLStreamReader(new FileReader("sample1.xml"));
+			String data = Files.readString(Path.of(args[0]));
+			
 			MyStack<String> stack = new MyStack<>();
 			MyQueue<String> errorQ = new MyQueue<>();
-			MyQueue<String> extrasQ = new MyQueue<>();			
-			int index = 0;
+			MyQueue<String> extrasQ = new MyQueue<>();
 			
-			while (reader.hasNext()) {
-				if (reader.isStartElement() && reader.isEndElement()) {
-					reader.nextTag();
-					index = index + 1;
+			boolean passedTests = true;
+			int index = 1;
+			
+			// Grabs all of the tags
+			ArrayList<String> allMatches = new ArrayList<String>();
+			 Matcher m = Pattern.compile("<([^>]+)>")
+			     .matcher(data);
+			 while (m.find()) {
+			   allMatches.add(m.group());
+			 }
+			 allMatches.remove(0);
+			// Gets the tags and then removes it to only its name
+			for (int i = 0; i < allMatches.size(); i++) {
+				String val = allMatches.get(i);
+				boolean selfContain = false;
+				if (val.charAt(val.length() - 2) == '/') {
+					selfContain = true;
 				}
-				
-				if (reader.isStartElement() && !(reader.isEndElement())) {
-					stack.push(reader.getAttributeType(index));
-					index = index + 1;
-				}
-				
-				if (reader.isEndElement()) {
-					if (reader.getAttributeType(index) == stack.peek()) {
-						stack.pop();
-					}
-					else if (reader.getAttributeType(index) == errorQ.peek()) {
-						errorQ.dequeue();
-						index = index + 1;
-					}
-					else if (stack.size() == 0) {
-						errorQ.enqueue(reader.getAttributeType(index));
-					}
-					else {
-						stack.search(reader.getAttributeType(index));
-						if (stack.contains(reader.getAttributeType(index))) {
-							while (true) {
-								errorQ.enqueue(stack.pop());
-							}
-						}
-					}
+				allMatches.set(i, val.split(" ")[0]);
+				allMatches.set(i, allMatches.get(i).substring(1));
+				allMatches.set(i, allMatches.get(i).replace(">", ""));
+				// Adds an identifier for self closing tags
+				if (selfContain) {
+					allMatches.set(i, '|' + allMatches.get(i));
 				}
 			}
+			/*
+			for (String i : allMatches) {
+				System.out.println(i);
+			}
+			*/
+
+
+			// Iterates through all tags
+			for (String i : allMatches) {
+				
+				// Opening Tags
+				if ((!i.contains("/")) && !i.contains("|")) {
+					stack.push(i);
+					// Continues with the next item
+					index++;
+					
+				}
+				// Closing Tags
+				else if (i.contains("/")) {
+					String name = i.substring(1);
+					String peeker = null;
+					if (!errorQ.isEmpty()) {
+						peeker = errorQ.peek();
+					}
+					// Success
+					if (name.equals(stack.peek())) {
+						stack.pop();
+					}
+					// No Opening Equivilent
+					else if (stack.isEmpty()) {
+						passedTests = false;
+						System.out.println("Error found near tag: "+index+", </"+name+"> is not constructed correctly");
+						errorQ.enqueue(name);
+					}
+					// Equal to last error
+					else if (name.equals(peeker)) {
+						passedTests = false;
+						System.out.println("Error found near tag: "+index+", </"+name+"> is not constructed correctly");
+						errorQ.dequeue();
+					
+					}
+					else {
+						passedTests = false;
+						boolean hasItem = stack.contains(name);
+						System.out.println("Error found near tag: "+index+", </"+name+"> is not constructed correctly");
+						if (hasItem) {
+							while (true) {
+								String curItem = stack.pop();
+								errorQ.enqueue(curItem);
+								if (curItem.equals(name)) break;
+							}
+						} else {
+							extrasQ.enqueue(name);
+						}
+						
+						
+					}
+					
+					// Continues with the next item
+					index++;
+				}
+				// Self Closing Tags
+				else  {
+					// Continues with the next item
+					index++;
+				}
+				
+			}
+			if (passedTests) System.out.println("XML Document is constructed properly.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
+	
 }
